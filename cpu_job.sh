00cdbc24ed14e544a88363f14b1e1dde8bac2b53
Kasper Hagen Slagstad <kaspehs@login-3.betzy>
2026-01-21 10:25:25 +0100

    supercomputer commit

---
+++ cpu_job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --job-name=test_run_1
+#SBATCH --account=nn9352k
+#SBATCH --time=00:05:00
+#SBATCH --partition=preproc
+#SBATCH --ntasks=1 --cpus-per-task=32
+#SBATCH --mem-per-cpu=2G
+#SBATCH --nodes=1
+
+#SBATCH --output=output/%j.txt
+#SBATCH --error=error/%j.txt
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export PYTHONUNBUFFERED=1
+
+
+# it is good to have the following lines in any bash script
+set -o errexit  # make bash exit on any error
+set -o nounset  # treat unset variables as errors
+
+module load Python/3.10.8-GCCcore-12.2.0
+
+# Set the ${PS1} (needed in the source of the virtual environment for some Python versions)
+export PS1=\$
+
+# activate the virtual environment
+source $HOME/ml-env/bin/activate
+
+python HNN.py --config HNNrunconfigs/pirate_final.yml
