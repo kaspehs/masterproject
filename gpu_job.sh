d8ce502eceb2d126f911084e21c664e975104230
Kasper Hagen Slagstad <kaspehs@uan02.head.cm.americas.sgi.com>
2026-02-02 15:30:10 +0100

    changes

--- gpu_job.sh
+++ gpu_job.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=gpu_test
 #SBATCH --account=nn9352k
 #SBATCH --partition=accel
-#SBATCH --time=00:15:00
+#SBATCH --time=00:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:1
 
