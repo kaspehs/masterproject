f4ef0a3ce45110ad67fe4ae2539bef73c54eaf9e
Kasper Hagen Slagstad <kaspehs@login-2.betzy>
2026-04-15 13:47:12 +0200

    baseline config changes

--- bashscripts_betzy/gpu_job.sh
+++ bashscripts_betzy/gpu_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=betzy_gpu_train
 #SBATCH --account=nn9352k
 #SBATCH --partition=accel
-#SBATCH --time=02:00:00
+#SBATCH --time=04:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=4G
